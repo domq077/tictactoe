05fa157e26ca2f297de6d848c0e72700ec014f33
domq077 <dominik.smutek.51@onet.pl>
2021-03-24 18:50:49 +0100

    create Board and Game compenents

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Square from './Square';
 
-const Board = () => <div>Board</div>
+const Board = ({squares, onClick}) => (
+    <div>
+        <Square value='1' onClick={() => onClick('1')} />
+        <Square value='2' onClick={() => onClick('1')} />
+        <Square value='3' onClick={() => onClick('1')} />
+        <Square value='4' onClick={() => onClick('1')} />
+        <Square value='5' onClick={() => onClick('1')} />
+        <Square value='6' onClick={() => onClick('1')} />
+        <Square value='7' onClick={() => onClick('1')} />
+        <Square value='8' onClick={() => onClick('1')} />
+        <Square value='9' onClick={() => onClick('1')} />
+    </div>
+);
 
 export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Board from './Board';
 
-const Game = () => <div>Game</div>
+const Game = () => {
+    
+    const handleClick = () => {
+
+    }
+
+    const jumpTo = () => {
+
+    }
+
+    const renderMove = () => {
+        
+    }
+
+    return (
+        <div>
+            <Board onClick={handleClick}/>
+        </div>
+    );
+}
 
 export default Game;
